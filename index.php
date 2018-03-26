4ae8be578796585272795c8e71956c370c88e847
Josh <joseldridge15@gmail.com>
2018-03-26 14:23:44 -0400

    Joke Emporium

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>One Awesome Title</title>
+    <title>Joke Emporium</title>
 </head>
 <body>
 <ul class="nav justify-content-center">
@@ -25,7 +25,23 @@
         <a class="nav-link disabled" href="#">Disabled</a>
     </li>
 </ul>
-
+<form>
+    <div class="form-group">
+        <label for="emailInput">Email address</label>
+        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll share your email with everyone. Praise spam!</small>
+    </div>
+    <div class="form-group">
+        <label for="passwordInput">Password</label>
+        <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+        <small id="passwordHelp" class="form-text text-muted">Totally not secure.</small>
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Cash me outside</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
