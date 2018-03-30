fd000083dad94dc93c17816c8ebc726f82534d64
Cameron Sprowls <poiklasderf@gmail.com>
2018-03-30 12:16:45 -0400

    Made it rain.

--- index.php
+++ index.php
@@ -10,6 +10,22 @@
     <link rel="shortcut icon" href="./img/favicon.ico" />
     <title>Joke Emporium</title>
     <link rel="stylesheet" type="text/css" href="makeItRain.css">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="makeItRain.js" > </script>
+
+    <script>
+        //$("#rain").makeItRain();
+
+        $(document).ready(function(){
+            //$("#rain").click(function(){makeItRain()});
+            $("#rain").makeItRain();
+        });
+    </script>
 </head>
 <body>
 <ul class="nav justify-content-center">
@@ -37,17 +53,12 @@
         <input type="password" class="form-control" id="passwordInput" placeholder="Password">
         <small id="passwordHelp" class="form-text text-muted">Totally not secure.</small>
     </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label id = 'rain' class="form-check-label" for="exampleCheck1">Cash me outside</label>
-    </div>
+
+    <button id="rain" type="button" class="btn btn-primary">Cash me outside</button>
+    <br><br>
+
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="makeItRain.js" > </script>
+
 </body>
 </html>
