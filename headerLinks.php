438c1648708591bb3e137a0fcc1ddb34592432d1
rodgerskei <32454700+rodgerskei@users.noreply.github.com>
2018-04-15 14:41:34 -0400

    Added viewAnnouncements, updated header links

--- headerLinks.php
+++ headerLinks.php
@@ -6,6 +6,12 @@
         <a class="nav-link" href="./blackboard.php?course_id=_3_1">Blackboard Grades</a>
     </li>
     <li class="nav-item">
+        <a class="nav-link" href="./checkAnnouncements">Check Announcements</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="./viewAnnouncements">View Announcements</a>
+    </li>
+    <li class="nav-item">
         <a class="nav-link" href="./logout.php">Logout</a>
     </li>
 </ul>
